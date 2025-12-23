4b7fd6315df2e13ac83cb53f995a48eff1840216
Sheng Pang <shengdevelop@gmail.com>
2025-12-22 20:55:46 -0800

    update ceiling saving data structure

--- mybittools/src/app/ceilingSaving.tsx
+++ mybittools/src/app/ceilingSaving.tsx
@@ -10,34 +10,57 @@
  * should saving: it will calculate the should saving by substracting the total cost from the ceiling cost
  * 
  */
+interface DailySpend {
+    date: string;
+    items: {
+        [key: string]: number;
+    }
+}
+
 export default function ceilingSaving() {
-    const spend: Array<{ date: string;[key: string]: string | number }> = [
+    const spend: Array<DailySpend> = [
         {
             date: "2025-12-21",
-            "胃药 cvs": 20.94,
-            "可乐，小饼干 shell 加油站": 6.02,
-            "水煮鱼 san mateo": 49.78,
-            "充电": 18.93,
-            "总计": 0,
-            "向上取整总计花费": 0,
-            "向上取整总计应存入": 0
-        }
+            items: {
+                "胃药 cvs": 20.94,
+                "可乐，小饼干 shell 加油站": 6.02,
+                "水煮鱼 san mateo": 49.78,
+                "充电": 18.93,
+                "总计": 0,
+                "向上取整总计花费": 0,
+                "向上取整总计应存入": 0
+            }
+        },
+        {
+            date: "2025-12-22",
+            items: {
+                "fastrack": 120,
+                "日定存": 34,
+                "过桥": 8,
+                "彩票 power fantasy": 3,
+                "芬达菠萝": 1.5,
+                "总计": 0,
+                "向上取整总计花费": 0,
+                "向上取整总计应存入": 0
+            }
+        },
+
     ]
 
     const keyWontCount: Array<string> = ["date", "总计", "向上取整总计花费", "向上取整总计应存入"];
-    spend.forEach((item) => {
+    spend.forEach((eachDay) => {
         let currentCeilingTotalCost: number = 0;
         let currentTotalCost: number = 0;
-        Object.keys(item).forEach((key) => {
-            if (!keyWontCount.includes(key) && typeof (item[key]) == "number") {
-                const ceilingCost = Math.ceil(item[key]);
+        Object.keys(eachDay.items).forEach((key) => {
+            if (!keyWontCount.includes(key) && typeof (eachDay.items[key]) == "number") {
+                const ceilingCost = Math.ceil(eachDay.items[key]);
                 currentCeilingTotalCost += ceilingCost;
-                currentTotalCost += item[key];
+                currentTotalCost += eachDay.items[key];
             }
         })
-        item["总计"] = currentTotalCost;
-        item["向上取整总计花费"] = currentCeilingTotalCost;
-        item["向上取整总计应存入"] = Math.ceil(item["向上取整总计花费"] - item["总计"]);
-        console.log(`今天是${item.date}, 总计花费${item["总计"]}, 向上取整应该存: ${item["向上取整总计应存入"]}`);
+        eachDay.items["总计"] = currentTotalCost;
+        eachDay.items["向上取整总计花费"] = currentCeilingTotalCost;
+        eachDay.items["向上取整总计应存入"] = Math.ceil(eachDay.items["向上取整总计花费"] - eachDay.items["总计"]);
+        console.log(`今天是${eachDay.date}, 总计花费${eachDay.items["总计"]}, 向上取整应该存: ${eachDay.items["向上取整总计应存入"]}`);
     });
 }
