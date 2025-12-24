faaef983bbf63cbf1bb4ecca1d26e2ba9ccb761b
Sheng Pang <shengdevelop@gmail.com>
2025-12-24 00:19:40 -0800

    comment out pg

---
+++ db/cleanData/index.ts
@@ -0,0 +1,13 @@
+// import { Client } from "pg";
+
+// const client = new Client({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "postgres",
+//     password: "postgres",
+//     port: 5432,
+// });
+
+// client.connect();
+
+// console.log("clean data");
