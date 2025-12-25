d0a565dcb9f6607128774d07d9847c710e81f43c
Sheng Pang <shengdevelop@gmail.com>
2025-12-24 21:37:11 -0800

    update render, add every day total to screen

--- mybittools/src/app/expense/page.tsx
+++ mybittools/src/app/expense/page.tsx
@@ -29,23 +29,30 @@ export default function ExpensePage() {
     }, {} as GroupedExpenses);
 
     // 渲染分组后的数据
-    const spendingDisplay = Object.entries(groupedByDate).map(([date, items]) => (
-        <div key={date} style={{ marginBottom: '20px' }}>
-            <h3>{date}</h3>
-            <ul>
-                {items.map((row) => (
-                    <li key={row.id}>
-                        {row.item} | ${row.amount}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    ));
+    const spendingDisplay = Object.entries(groupedByDate).map(([date, items]) => {
+        let totalSpending = 0
+        return (
+            <div key={date} style={{ marginBottom: '20px' }}>
+                <h3>{date}</h3>
+                <ul>
+                    {
+                        items.map((row) => {
+                            totalSpending += Number(row.amount);
+                            return <li key={row.id}>
+                                {row.item} | ${row.amount}
+                            </li>
+                        })
+                    }
+                    <li>总计: ${totalSpending}</li>
+                </ul>
+            </div>
+        )
+    });
 
     useEffect(() => {
         fetch("http://localhost:3000/api/expenseTracker")
             .then((res) => res.json())
-            .then((data) => setSpendData(data));
+            .then((data) => setSpendData(data.reverse())); // reverse the data in db new record is last record, in ui is first record
     }, []);
 
     return (
