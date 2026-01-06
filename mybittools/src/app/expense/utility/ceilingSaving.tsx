7befdfc53716e782e368529f16f54836eace1fd5
Sheng Pang <shengdevelop@gmail.com>
2026-01-06 09:50:26 -0800

    since I moved temp expense data to json, I have to read json file here

--- mybittools/src/app/expense/utility/ceilingSaving.tsx
+++ mybittools/src/app/expense/utility/ceilingSaving.tsx
@@ -10,6 +10,7 @@
  * should saving: it will calculate the should saving by substracting the total cost from the ceiling cost
  * 
  */
+import fs from "fs";
 interface DailySpend {
     date: string;
     items: {
@@ -18,79 +19,12 @@ interface DailySpend {
 }
 
 export default function ceilingSaving() {
-    const spend: Array<DailySpend> = [
-        {
-            date: "2025-12-21",
-            items: {
-                "胃药 cvs": 20.94,
-                "可乐，小饼干 shell 加油站": 6.02,
-                "水煮鱼 san mateo": 49.78,
-                "充电": 18.93,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-        {
-            date: "2025-12-22",
-            items: {
-                "fastrack": 120,
-                "日定存": 34,
-                "过桥": 8,
-                "彩票 power fantasy": 3,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-        {
-            date: "2025-12-23",
-            items: {
-                "google one": 1.99,
-                "收到支票 Transunion": +124.75,
-                "日定存": 35,
-                "假装过桥存": 8,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-        {
-            date: "2025-12-24",
-            items: {
-                "power fantasy": 3,
-                "寿司 橙子": 138,
-                "假装过桥存 （8块钱银行decilined）": 20,
-                "日定存": 36,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-        {
-            date: "2025-12-25",
-            items: {
-                "泰诺 小熊 红牛": 28.92,
-                "假装过桥存": 8,
-                "日定存": 37,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-        {
-            date: "2025-12-26",
-            items: {
-                "充电": 13.85,
-                "兰州牛肉面": 85.77,
-                "* 问自己借": 500,
-                "日定存": 38,
-                "总计": 0,
-                "向上取整总计花费": 0,
-                "向上取整总计应存入": 0
-            }
-        },
-    ]
+    // read from temp data (my expense)
+    const spend: Array<DailySpend> = JSON.parse(fs.readFileSync(
+        "../../expense/tempData/myExpense.json", "utf-8"
+    ))
+
+    console.log(spend)
 
     const keyWontCount: Array<string> = ["date", "总计", "向上取整总计花费", "向上取整总计应存入"];
     spend.forEach((eachDay) => {
